83b7eb8d490e261ec1b8607ea11c460bc2db2957
abelmaniquis <abelmaniquis@gmail.com>
2016-05-30 21:33:08 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+Create these constructor functions: 
+questions, ingredients, pantry
+
+use these constructors to create objects 
+representing the bartender's questions, 
+ingredients and pantry.
+
+-Create a Bartender object.
+
+-Create a constructor for a bartender object,
+-Give the bartender a createDrink method
+
+-Add in logic to your Bartender object 
+so that when the user submits their
+preferences, the drink is created and
+displayed in a UI.
+*/
+
+var Bartender = function(questions){
+    this.questions = questions;
+};
+
+Bartender.prototype.makeDrinkg = function(){
+    
+};
+
+var Questions = function(){
+};
+
+
+var Pantry = function(ingredients){}
+    
+
+var Ingredients = function(){
+    
+}
